919605431edbe66ffa8e663f18c0d50afb9bc6b4
mingzhou8023 <1309933642@qq.com>
2025-04-13 00:12:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import App from './App'
+// 导入uviewPlus组件
+import uviewPlus from '@/uni_modules/uview-plus'
+// 导入mock.js文件
+import './api/mock.js'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
